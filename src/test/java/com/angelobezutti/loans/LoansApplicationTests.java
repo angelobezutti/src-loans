96faab08e3b5951dfc5dbe7162b9e00987d255c0
angelobezutti <angeloantoniobzt@gmail.com>
2024-04-17 19:53:28 -0300

    Add Project

---
+++ src/test/java/com/angelobezutti/loans/LoansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angelobezutti.loans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
